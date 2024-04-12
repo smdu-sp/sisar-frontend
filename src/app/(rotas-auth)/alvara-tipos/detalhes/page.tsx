c62f0fd36dc79d2f5acedfb9fea775aa6b34aa1f
Victor Abreu <aalevictor@gmail.com>
2024-04-12 14:00:00 -0300

    remover arquivos

--- src/app/(rotas-auth)/alvara-tipos/detalhes/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import AlvaraTipoDetalhes from './[id]/page';
-
-export const dynamicParams = true
-
-export default function AlvaraTipoNovo(props: any) {
-    return AlvaraTipoDetalhes(props);
-}
