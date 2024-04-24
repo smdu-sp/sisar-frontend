a0d3330e3015a563b2bf65d3e1079107043909c4
Victor Abreu <aalevictor@gmail.com>
2024-04-24 12:50:47 -0300

    authoptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,4 +2,4 @@ import { authOptions } from "@/shared/auth/authOptions";
 import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST, authOptions };
+export { handler as GET, handler as POST };
