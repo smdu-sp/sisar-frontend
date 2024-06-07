4224af993946c3501704847db5282261711ef01c
Gustavo08Mendes <gustavo08.mendes@gmail.com>
2024-06-07 16:16:49 -0300

    Removido detalhes de Admissibilidade

--- src/app/(rotas-auth)/admissibilidade/detalhes/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import AdmissibilidadeDetalhes from './[id]/page';
-
-export const dynamicParams = true
-
-export default function InicialNovo(props: any) {
-    return AdmissibilidadeDetalhes(props);
-}
