bb7c625d7d2c5b8d0aeead5277e5fef84b0348a2
Victor Abreu <aalevictor@gmail.com>
2024-06-10 21:24:15 -0300

    favico layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,6 +105,13 @@ export default function RootLayout({children}:{children: React.ReactNode}) {
                 <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
                 <title>{process.env.NEXT_PUBLIC_PROJECT_NAME}</title>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#eeeeee" />
+                <meta name="theme-color" content="#eeeeee" />
               </head>
               <body>
                 <AuthSessionProvider>
