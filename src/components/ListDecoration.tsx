34d03999a92ac2735587eebfc06ec94b9f64ee9a
gumends <gustavo08.mendes@gmail.com>
2024-06-14 15:42:06 -0300

    adaptado para sistema de avisos

--- src/components/ListDecoration.tsx
+++ src/components/ListDecoration.tsx
@@ -1,13 +1,13 @@
 import { Badge, ListItemDecorator } from "@mui/joy";
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import PendingActionsIcon from '@mui/icons-material/PendingActions';
-
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 
 export default function ListDecoration(props: any) {
     return (
         <ListItemDecorator>
-        <Badge badgeContent={props.valor} color={props.tipo == 1 ? 'primary' : 'warning'} max={9}>
-          { props.tipo == 1 ? <PeopleAltIcon /> : <PendingActionsIcon /> }
+        <Badge badgeContent={props.valor} color="success" max={9}>
+          { props.tipo == 1 ? <PeopleAltIcon /> : props.tipo == 0 ? <PendingActionsIcon /> : <NotificationsActiveIcon />}
         </Badge>
       </ListItemDecorator>
     );
