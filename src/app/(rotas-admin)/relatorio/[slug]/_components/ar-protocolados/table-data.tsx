91b9a5ca932d521642d37110f496e88cac6eac5b
David Diniz Dos Santos <ddavid.diniz@gmail.com>
2025-01-30 11:15:18 -0300

    refactor: remove unused mock data import from table-data component

--- src/app/(rotas-admin)/relatorio/[slug]/_components/ar-protocolados/table-data.tsx
+++ src/app/(rotas-admin)/relatorio/[slug]/_components/ar-protocolados/table-data.tsx
@@ -8,7 +8,6 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { mockDataTable } from '../../_constants/mockdata';
 
 function createDadosAnuais(
 	ano: number,
